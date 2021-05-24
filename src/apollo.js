527777aec21c848b8920b54e4a2b911ed4b3412f
NaHyeon <nahyeonee99@gmail.com>
2021-05-24 21:06:28 +0900

    #1.1

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,13 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+const cache = new InMemoryCache();
+const link = new createHttpLink({
+  uri: "http://localhost:4000/",
+});
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000",
-  cache: new InMemoryCache(),
+  cache: cache,
+  link: link,
 });
 
 export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,22 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => <h1>Home</h1>;
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
