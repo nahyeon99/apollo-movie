26b0c47eb3a25d163ee8e09eb2d815d1ee426dfa
NaHyeon <nahyeonee99@gmail.com>
2021-05-24 21:47:11 +0900

    #1.2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,29 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => "Detail";
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  console.log(loading, data);
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
